425a83abcdb1c36387f21bd32963f3b2ce873d3e
BrenoLuna0 <breno.luna_@hotmail.com>
2020-06-03 17:03:23 -0300

    adição de novo buildpack para compilar no heroku

--- src/routes.js
+++ src/routes.js
@@ -1,29 +1,30 @@
-const express = require('express');
+const express = require("express");
 const routes = express.Router();
 
-const TitularController = require('./controller/TitularController');
-const ConvidadoController = require('./controller/ConvidadoController');
-const AgendaController = require('./controller/AgendaController');
-const AgendaConvidadoController = require('./controller/AgendaConvidadoController');
-const auth = require('./services/auth');
-const gerarId = require('./services/geradorDeId');
+const TitularController = require("./controller/TitularController");
+const ConvidadoController = require("./controller/ConvidadoController");
+const AgendaController = require("./controller/AgendaController");
+const AgendaConvidadoController = require("./controller/AgendaConvidadoController");
+const auth = require("./services/auth");
+const gerarId = require("./services/geradorDeId");
 
-
-routes.get('/', (req,res)=>{
-    res.send('Tudo Certo nada Errado');
+routes.get("/", (req, res) => {
+  res.send(
+    "Posso pressentir o perigo e o caos, e ninguém agora vai me amedrontar"
+  );
 });
 
-routes.get('/titular', auth, TitularController.index);
-routes.post('/titular', TitularController.login);
+routes.get("/titular", auth, TitularController.index);
+routes.post("/titular", TitularController.login);
 
-routes.get('/convidado/:sociCodigo', auth, ConvidadoController.index);
-routes.post('/convidado', auth, ConvidadoController.create);
+routes.get("/convidado/:sociCodigo", auth, ConvidadoController.index);
+routes.post("/convidado", auth, ConvidadoController.create);
 
-routes.post('/agenda', auth, AgendaController.create);
+routes.post("/agenda", auth, AgendaController.create);
 
-routes.post('/agendaConvidado', auth, AgendaConvidadoController.create);
+routes.post("/agendaConvidado", auth, AgendaConvidadoController.create);
 
-routes.get('/gerarIdConvidado', auth, gerarId.gerarId);
-routes.get('/gerarIdAgenda', auth, gerarId.gerarIdAgenda);
+routes.get("/gerarIdConvidado", auth, gerarId.gerarId);
+routes.get("/gerarIdAgenda", auth, gerarId.gerarIdAgenda);
 
 module.exports = routes;
