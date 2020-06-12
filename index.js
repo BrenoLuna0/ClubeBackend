fcd8c3f827accceed2b4725a72dd10f4c26b3d32
BrenoLuna0 <breno.luna_@hotmail.com>
2020-06-12 13:19:56 -0300

    adição da política de cors

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.use('/api', require('./src/routes'));
-app.listen(process.env.PORT || 3013, ()=>{
-    console.log('Servidor ouvindo na porta 3013');
+app.use("/api", require("./src/routes"));
+app.listen(process.env.PORT || 3013, () => {
+  console.log("Servidor ouvindo na porta 3013");
 });
